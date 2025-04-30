1ad87ceec3d375996a9965b5f35b29714c338f71
김영훈 <joseph0926@naver.com>
2025-04-30 14:21:34 +0900

    chore.delete: 사용안하는 파일 삭제

--- src/schemas/comment.schema.ts
+++
@@ -1,8 +0,0 @@
-import { Side } from '@prisma/client';
-import { z } from 'zod';
-
-export const createCommentSchema = z.object({
-  nickname: z.string().min(1).max(30),
-  content: z.string().min(1).max(1000),
-  side: z.nativeEnum(Side),
-});
